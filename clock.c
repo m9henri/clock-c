8555d32ddc611519233624a8ff19cb42f844afc1
m9henri <missolhenri@gmail.com>
2026-08-18 21:50:43 +0200

    added choose_mode() and timer_options()

--- clock.c
+++ clock.c
@@ -1,6 +1,72 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main()
+char choose_mode(void);
+void timer_options();
+
+int main(void)
 {
-    printf("Hello World!");
+    switch (choose_mode())
+    {
+        case 't':
+            timer_options();
+            break;
+        case 's':
+            break;
+    }
+}
+
+char choose_mode()
+{
+    char mode;
+
+    printf("What mode do you want to choose?\n");
+    printf(" - Timer (t)\n");
+    printf(" - Stopwatch (s) (doesnt work)\n> ");
+
+    scanf(" %c", &mode);
+
+    return mode;
+}
+
+void stopwatch()
+{
+
+}
+
+void timer(unsigned int ms)
+{
+    printf("%u", ms);
+}
+
+void timer_options()
+{
+    char unit;
+    unsigned short input; // memory management 🤑
+    unsigned int timems = 0;
+    char inputchar;
+    char whilebuffer = 'a';
+
+    system("clear");
+    while('a' == whilebuffer)
+    {
+        printf("What unit of time do you choose? (s,m,h)\n");
+        scanf(" %c", &unit);
+        printf("How much of these units do you want to add? ");
+        scanf("%hu", &input);
+
+        // calculating time to wait
+        if('s' == unit) { timems += (input * 1000); }
+        if('m' == unit) { timems += (input * 60000); }
+        if('h' == unit) { timems += (input * 3600000); }
+
+        printf("Do you want to add another unit? (y/n) ");
+        scanf(" %c", &inputchar);
+
+        if('n' == inputchar) { whilebuffer = 'b'; }
+    }
+
+    // Continue to timer after while loop has been broken
+    timer(timems);
 }
